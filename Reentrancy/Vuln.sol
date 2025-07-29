b9e404586a7eed20dee2c12e3060ab2dab62cfec
GXT <tudorghetiu1@gmail.com>
2025-07-29 12:54:48 +0300

    Create Vuln.sol
    Intentionally vulnerable staking contract. The vulnerability consists of the incorrect order of operations in the withdraw function - the state is updated only after the transfer is made. This allows an attacker to recursively call the withdraw function until the balance is drained.

---
+++ Reentrancy/Vuln.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0; //old vulnerable version 
+
+contract VulnerableStaking {
+    mapping(address => uint256) public balances;
+
+    constructor() payable{}
+
+    function stake() public payable{
+        balances[msg.sender] += msg.value;
+    }
+
+    // Vulnerable withdraw function 
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        //Sends Ether before updating state
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Transfer failed");
+        balances[msg.sender] -= amount;
+    }
+
+    receive() external payable {}
+}
